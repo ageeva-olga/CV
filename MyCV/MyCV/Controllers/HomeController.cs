780bee116a83b59451dfb1cf0a6d87edd103024c
ageeva-olga <olga.ageeva.999@mail.ru>
2020-10-02 21:51:33 +0300

    Добавила весь остальной код

---
+++ MyCV/MyCV/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using MyCV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new PersonalInfoModel();
+            return View(model);
+        }
+
+    }
+}
---
+++ MyCV/MyCV/Models/PersonalInfoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCV.Models
+{
+    public class PersonalInfoModel
+    {
+        public string Name => "Olga";
+        public string Surname => "Ageeva";
+        public string Phone => "+79200513315";
+        public string Email => "olga.ageeva.999@mail.ru";
+
+        public string FullName => $"{Name} {Surname}";
+    }
+}
